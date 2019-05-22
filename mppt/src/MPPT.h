ba5b1a250ae9bb19f533b60369eb64ddefe73327
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-22 14:32:27 +0200

    Update MPPT.h

--- mppt/src/MPPT.h
+++ mppt/src/MPPT.h
@@ -2,6 +2,7 @@
 
 float I, V, P; // Variables for Current, Voltage and Power
 
+Serial pc2(USBTX, USBRX);
 
 /* Maximum Power Point Tracker Class
 Control the duty cycle of a DC-DC converter switch using PWM,
@@ -22,16 +23,20 @@ class MPPT{
     MPPT(PinName I_pin,PinName V_pin,PinName PWM_pin,PinName D_pin);
     float readI(); // read from current sensor
     float readV(); // read from voltage sensor
-    void PerturbObserve(); // Perturb and Observe MPP Tracking algorithm
+    float readP(); // return last calculated power
+
+    /* Perturb and Observe MPP Tracking algorithm.
+    Optional target power argument in case a lower power point needs to be tracked.*/
+    void PerturbObserve(float Target); 
+
+    void pause(); // stop tracking power point, open PWM switch
 };
 
 MPPT::MPPT(PinName I_pin,PinName V_pin,PinName PWM_pin,PinName D_pin){
-  
   CurrentSensor = new AnalogIn(I_pin);
   VoltageSensor = new AnalogIn(V_pin);
   PwmOutput = new PwmOut(PWM_pin);
   DutyOutput = new AnalogOut(D_pin);
-
 
   Perturbation = 0.01;// the change in the duty cycle
   PwmOutput->period_us(100); // set PWM frequency to 10kHz (1/100us = 10kHz)
@@ -45,24 +50,51 @@ float MPPT::readV(){
   return VoltageSensor->read(); // return read voltage
 }
 
-void MPPT::PerturbObserve(){
+float MPPT::readP(){
+  return PreviousPower; // return last calculated power
+}
+
+void MPPT::PerturbObserve(float Target = 0){
   //observe
   I = CurrentSensor->read(); // read current sensor
   V = VoltageSensor->read(); // read voltage sensor
   P = I*V; // calculate power
 
-  //perturb
-  if (P < PreviousPower) { // if previous perturbation resulted in loss of power
-    Perturbation = -Perturbation; // reverse perturbation direction
+  /*perturb*/
+  if (Target == 0) { // No target given -> Track MPPT
+    if (P < PreviousPower) { // if previous perturbation resulted in loss of power:
+      Perturbation = -Perturbation; // reverse perturbation direction
+    }
+    
+  }
+  else { // Track target power
+    pc2.printf("Target power: %d mW\r\n",static_cast<int>(Target*1000));
+    if (P>Target){
+      Perturbation = -abs(Perturbation); // set perturbation to negative
+    }
+    else {
+      Perturbation = abs(Perturbation); // set perturbation to positive
+    }  
   }
   DutyCycle = DutyCycle+Perturbation; // Apply Perturbation
+  /*end perturb*/
 
   // Constrain duty cycle to be in between 0 and 1
   if (DutyCycle < 0) {DutyCycle = 0;}
   else if (DutyCycle > 1){DutyCycle = 1;}
   
-  DutyOutput->write(DutyCycle); // write Duty Cycle as floating point
+  DutyOutput->write(DutyCycle); // write Duty Cycle
   PwmOutput->write(DutyCycle); // write PWM
   
   PreviousPower = P; // store calculated power  
+}
+
+void MPPT::pause(){
+  DutyCycle = 0; // Set duty cycle to zero
+  DutyOutput->write(DutyCycle); // write Duty Cycle
+  PwmOutput->write(DutyCycle); // Open MOSFET
+  I = CurrentSensor->read(); // read current sensor
+  V = VoltageSensor->read(); // read voltage sensor
+  P = I*V; // calculate power (should be zero)
+  PreviousPower = P;
 }
