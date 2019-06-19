0e11b99a82bd1c8ef57cae78f7bf51d87d3f308c
tomwemelsfelder <tomwemel@hotmail.com>
2019-06-19 14:01:55 +0200

    Removed PV switch
    Battery switch will now completely disconnect the batteries

--- mppt/src/ChargeController.h
+++ mppt/src/ChargeController.h
@@ -5,16 +5,18 @@
 
 float Ibat, Vload, Pbat; // Variables for current, voltage for battery and load, and power from battery
 float SoC; // Variable for State of Charge of the battery pack, given in percentages
-const float VbMax = 12; // Maximum Battery voltage (SoC = 100)   //// Need to verify value
+const float VbMax = 8.4; // Maximum Battery voltage (SoC = 100)   //// Need to verify value
 const int SoCMax = 95; // Maximum allowed battery State of Charge //// Need to verify value
 const int SoCDanger = 97; // Dangerously high SoC //// Need to verify value
 const int SoCMin = 20; // Minimum allowed battery SoC //// Need to verify value
-const float BatCap = 11; // [mAh] //// Need to verify value
+const float BatCap = 3800; // [mAh] //// Need to verify value
 const float IbatDanger = 2*BatCap/1000; // [A] maximum safe charging current
 const float IbatMax = IbatDanger*0.8;
 
-const float ChargingConst = 0.1; // Arbitrary constant used for calculating change in battery voltage
+const float ChargingConst = 0.001; // Arbitrary constant used for calculating change in battery voltage
 
+const float LOADCURRENT_SENSOR_MAX = 11; // [A] maximum current the sensor is able to sense properly
+const float BATVOLTAGE_SENSOR_MAX = 9; // [V] maximum voltage the sensor is able to sense properly
 
 Serial pc1(USBTX, USBRX);
 
@@ -38,12 +40,11 @@ class ChargeController{
 
         float PbatMax; // Maximum allowed battery charge power
 
-        DigitalOut *PvSwitch; // This switch can disconnect the PV system from the batteries and load
-        DigitalOut *BatSwitch; // This switch can disconnect the Batteries from the PV system, not allowing them to charge
+        DigitalOut *BatSwitch; // This switch can disconnect the Batteries, not allowing them to charge or discharge
 
     public:
         float Ppv; // Incoming power from solar panels
-        ChargeController(PinName PvSwPin, PinName BatSwPin);
+        ChargeController(PinName BatSwPin);
         void run(); // Execute charge controller algorithm
         int readControl(); // Allows the control signal to be read
         float readPload(); // Allows Pload to be read
@@ -51,14 +52,13 @@ class ChargeController{
 
 };
 
-ChargeController::ChargeController(PinName PvSwPin, PinName BatSwPin){
-    PvSwitch = new DigitalOut(PvSwPin);
+ChargeController::ChargeController(PinName BatSwPin){
     BatSwitch = new DigitalOut(BatSwPin);
     
     /* In the final code, these two values should be read via sensors
     However for this simulation, their values are set through software*/
-    Iload = 0.05; // [A] 
-    Vbat = 11.1; // [V]
+    //Iload = 0.05; // [A] 
+    Vbat = 7.4; // [V]
 };
 
 int ChargeController::readControl(){
@@ -87,68 +87,56 @@ void ChargeController::run(){
     PbatMax = IbatMax*Vbat; // Set maximum allowed battery charging power; to be read by main loop
 
     Vload = Vbat; // Load is in parallel with the batteries   
-    Pload = 0.15; // Calculate power used by the load (hard set to 150mW in this sim)
+    Pload = 20; // Calculate power used by the load (hard set to 20W in this sim)
 
+    /*  Might need to implement some control algorithm in case of low voltage/
+    if (SoC <= SoCMin){ // Battery charge is too low
+        LoadSwitch->write(open);
+    }
+    */
 
     if(Ibat >= IbatDanger){ // Battery charging current is dangerously high
         Control = 4; // Reset MPPT (open MOSFET and reset duty cycle to zero)
-        // open both switches
-        PvSwitch->write(open); // PV power must be cut off,
-        BatSwitch->write(open); // As the battery should quickly discharge to a safe level 
+        BatSwitch->write(open); // Disconnect battery 
 
     }
 
     if (SoC >= SoCDanger) { // Battery charge is dangerously high 
     
         Control = 1; // Pause MPPT (open MOSFET and pause algorithm)
-        // open both switches
-        PvSwitch->write(open); // PV power must be cut off,
-        BatSwitch->write(open); // As the battery should quickly discharge to a safe level      
+        BatSwitch->write(open); // Disconnect battery    
     }
 
     else if (SoC >= SoCMax && Ppv >= Pload) { // Both SoC and Ppv are too high
         Control = 2; // Tell main loop to read Pload. MPPTs must try to set Ppv equal to Pload
-        PvSwitch->write(close); // Keep PV connected to supply load power
-        BatSwitch->write(close); // Keep Battery connected to absorb excess current        
+        BatSwitch->write(close); // Keep battery connected to absorb excess current        
     }
 
     else if (Ibat >= IbatMax){ // Battery charging current is very high, but within limits
         Control = 3; // Track lower power point
-        
-        PvSwitch->write(close);
-        BatSwitch->write(close);
-        
+        BatSwitch->write(close);  // charge battery
     }
 
-    /*  Might need to implement some control algorithm in case of low voltage */
-    else if (SoC <= SoCMin){ // Battery charge is too low
-        Control = 0;
-        PvSwitch->write(close);
-        BatSwitch->write(close);
-    }
 
     else 
     {
-        Control = 0;
-        PvSwitch->write(close); 
-        BatSwitch->write(close);     
+        Control = 0; // Track MPP
+        BatSwitch->write(close);   // Charge battery 
     }
 
 
     /* Debugging */
-    pc1.printf("IbatMax = %d mA\r\n",static_cast<int>(IbatMax  * 1000));
-    pc1.printf("Ibat = %d mA\r\n",static_cast<int>(Ibat*1000));
-    if (PvSwitch->read() == open){pc1.printf("PvSwitch: open\r\n");}
-    else {pc1.printf("PvSwitch: closed\r\n");}
+    pc1.printf("IbatMax = %d A\r\n",static_cast<int>(IbatMax));
+    pc1.printf("Ibat = %d A\r\n",static_cast<int>(Ibat));
     if (BatSwitch->read() == open){pc1.printf("BatSwitch: open\r\n");}
     else {pc1.printf("BatSwitch: closed\r\n");}         
-    pc1.printf("Ppv: %d mW Pload: %d mW\r\n",static_cast<int>(Ppv*1000),static_cast<int>(Pload*1000)); 
+    pc1.printf("Ppv: %d W Pload: %d W\r\n",static_cast<int>(Ppv),static_cast<int>(Pload)); 
     pc1.printf("SoC: %d\r\n",static_cast<int>(SoC)); 
     /*          */
 
 
     // Calculate battery voltage
-    if (PvSwitch->read() && BatSwitch->read()){ // If charging is enabled
+    if (BatSwitch->read()){ // If charging is enabled
         Vbat = Vbat + (Ppv-Pload)*ChargingConst;
     }
     else
